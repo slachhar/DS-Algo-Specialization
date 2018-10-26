846bea8e3f002fcc4bfb45ae09b0038e8649fefe
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-10-26 10:35:22 +0530

    Selection Sort Algorithm

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -6,7 +6,29 @@ namespace SelectionSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] lst = { 4, 9, 7, 1, 3, 6, 5 };
+            int minimum;
+            for (int i = 0; i < lst.Length; i++)
+            {
+                minimum = lst[i];
+                int index = i;
+                for (int j = i; j < lst.Length; j++)
+                {
+                    if (lst[j] < minimum)
+                    {
+                        minimum = lst[j];
+                        index = j;
+                    }
+                }
+
+                int temp = lst[i];
+                lst[i] = minimum;
+                lst[index] = temp;
+            }
+
+            Console.WriteLine(string.Join(",", lst));
+            Console.ReadLine();
         }
     }
 }
